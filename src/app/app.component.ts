89bc0a8585406889fc1617afcdd4ceac6f0426db
Maicon Andraski <maiconm23@gmail.com>
2019-05-26 16:37:19 -0300

    css grid animation & styling 😎

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,20 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+const PROJECT_INFO = {
+  author: 'Maicon Andraski 😎',
+  version: '0.0.1 ',
+  lastUdate: '5/26/2019 📅',
+  GithubProfile: 'https://github.com/maiconm 👻'
+};
+const BOILERPLATE_NAME = '%cBoilerplate Ionic 4 📲';
+const LOG_STYLING = `
+    background-color: black;
+    font-size: 50px;
+    color: green;
+  `
+;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -14,6 +28,8 @@ export class AppComponent {
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    console.log(BOILERPLATE_NAME, LOG_STYLING);
+    console.table(PROJECT_INFO);
     this.initializeApp();
   }
 
--- src/app/tab2/css-grid/css-grid.component.ts
+++ src/app/tab2/css-grid/css-grid.component.ts
@@ -1,13 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IEmoji {
+  emoji: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-css-grid',
   templateUrl: './css-grid.component.html',
   styleUrls: ['./css-grid.component.scss'],
 })
 export class CssGridComponent implements OnInit {
-  public elements = [
-    '💩', '🔥', '🐒', '🌊', '😎', '🐂', '😼', '🍴', '⚡',
+  public emojis: IEmoji[] = [
+    {
+      emoji: '💩',
+      name: 'pile of poo'
+    },
+    {
+      emoji: '🔥',
+      name: 'fire'
+    },
+    {
+      emoji: '🐒',
+      name: 'monkey'
+    },
+    {
+      emoji: '🌊',
+      name: 'wave'
+    },
+    {
+      emoji: '😎',
+      name: 'smile sunglass'
+    },
+    {
+      emoji: '🐂',
+      name: 'ox'
+    },
+    {
+      emoji: '😼',
+      name: 'cat'
+    },
+    {
+      emoji: '🍴',
+      name: 'fork & knife'
+    },
+    {
+      emoji: '⚡',
+      name: 'bolt'
+    }
   ];
   constructor() { }
 
